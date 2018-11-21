18f6ca30549c910e69f8f2c407a51e8e6de341e9
Adam Champagne <achampagne@sussmanconsultants.com>
2018-11-21 14:58:01 -0500

    updates

--- app.js
+++ app.js
@@ -7,10 +7,11 @@ var passport      = require('passport');
 var session       = require('express-session');
 var bodyParser    = require('body-parser');
 var env           = require('dotenv').load();
+var routes        = require('./routes');
 var app           = express();
 LocalStrategy     = require("passport-local");
 User              = require("./models/user");
-var routes = require('./routes');
+
 
 // Database
 var db = require('./models');
@@ -21,6 +22,8 @@ app.use(logger('dev'));
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+app.use(routes);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -40,7 +43,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 
-app.use(routes);
+
 
 
 // catch 404 and forward to error handler
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 const path = require("path");
-var express = require('express');
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
 
 
 const apiRoutes = require("./api");
